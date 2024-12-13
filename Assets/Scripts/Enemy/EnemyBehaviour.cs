6a8536bd0c8dd9d1c3393d9985823800f4f5b6d4
Geraldine1610 <geraldine.sch2027@gmail.com>
2024-12-13 19:31:34 +0200

    player shoot & aliens detect shot
    - replaced enemies with alien photo
    - made player shoot projectile
    - made aliens detect shot
    - aliens & projectiles destroy themselves after shot
    - add 3 audios

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -4,5 +4,27 @@ using UnityEngine;
 
 public class EnemyBehaviour : MonoBehaviour
 {
-    
+    public AudioClip destructionSFX;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("Collided");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("Triggered");
+
+        if(collision.tag == "Laser")
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+            //1) what clip do you play, 2) where do you play it
+            AudioSource.PlayClipAtPoint(destructionSFX, transform.position);
+
+            
+        }
+        
+    }
 }
--- Assets/Scripts/Player/Shooter.cs
+++ Assets/Scripts/Player/Shooter.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    public GameObject projectilePrefab;
+    public AudioSource sfxPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
 
+    void Shoot()
+    {
+        //instantiate: copy the object
+        //where do I want it to come out? from the player:
+        // the script is on the player, so transform.position
+        //gives the player's position
+        //third property is rotation. no rotation, that's why:
+        //Quaternion.identity (keep it as it is)
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        sfxPlayer.Play();
+        
     }
 }
