0b01f6548ab51009982e11ca8f72f0c6568cf307
Geraldine1610 <geraldine.sch2027@gmail.com>
2024-12-14 19:58:30 +0200

    Change Position of Projectile
    Change position of projectile based on Santa's walking direction:
    1) projectile comes out of the wand; adjusted y-position of projectile
    2) adjusted x-position of projectile:
    Santa walks to the right: projectile appears to the right sprite
    Santa walks to the left: projectile appears to be to the left of the sprite
    => Bug: projectile stills shows up to the right even though Santa walks to the left

--- Assets/Scripts/Player/Shooter.cs
+++ Assets/Scripts/Player/Shooter.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+//using UnityEngine.Windows;
 
 public class Shooter : MonoBehaviour
 {
@@ -10,10 +13,22 @@ public class Shooter : MonoBehaviour
 	// Reference to the AudioSource component on the player
 	public AudioSource sfxPlayer;
 
+    // Get width of Santa to locate projectile
+    public SpriteRenderer playerSprite;
+    public float playerSpriteHalfWidth;
+    public float playerSpriteQuarterHeight = 0.9f;
+    Vector3 projectilePosition;
+
+    public float direction;
+
+    public float rightScreenEdge;
+    public float leftScreenEdge;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
@@ -26,10 +41,24 @@ public class Shooter : MonoBehaviour
     }
 
     void Shoot()
-    {	
-		// We instantiate the prefab at the same position as the player,
+    {
+        float inputHl = Input.GetAxis("Horizontal");
+        
+        if (inputHl > 0 && transform.position.x < rightScreenEdge - playerSpriteHalfWidth)
+        {
+            //Santa moves to the right - projectile shows up to the Sprite's right side
+            playerSpriteHalfWidth = 1.452f;
+        }
+        else if (inputHl < 0 && transform.position.x > leftScreenEdge + playerSpriteHalfWidth)
+        {
+            //Santa moves to the left - projectile shows up to the Sprite's left side
+            playerSpriteHalfWidth = -1.452f;
+        }
+
+        // We instantiate the prefab at the same position as the player,
         // since this script is on the player GameObject
-        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        projectilePosition = new Vector3(transform.position.x + playerSpriteHalfWidth, transform.position.y + playerSpriteQuarterHeight, transform.position.z);
+        Instantiate(projectilePrefab, projectilePosition, Quaternion.identity);
         sfxPlayer.Play();
     }
 }
