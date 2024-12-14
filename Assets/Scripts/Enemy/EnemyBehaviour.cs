c599207c84d7fb55169db388cd34b277102e6cec
Geraldine1610 <geraldine.sch2027@gmail.com>
2024-12-14 22:12:23 +0200

    Rename Aliens & Make Gifts fall and stop
    - Rename Aliens instances & alien prefab into gifts & gift
    - make flying gifts fall down after collision with laser (change rigidBodyType2D), make them stop once they hit the floor

--- Assets/Scripts/Enemy/EnemyBehaviour.cs
+++ Assets/Scripts/Enemy/EnemyBehaviour.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     public AudioClip destructionSFX;
+    Rigidbody2D gift;
+    public float floor = -4.6f;
 
     // physical simulation hits. For Unity to call this function, at least one of the colliding objects
-	// needs to have their RigidBody component set to "Dynamic" for Body Type
+    // needs to have their RigidBody component set to "Dynamic" for Body Type
     private void OnCollisionEnter2D(Collision2D collision)
     {
         print("I Collided!");
@@ -18,20 +20,38 @@ public class EnemyBehaviour : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("I was triggered!");
+        gift = GetComponent<Rigidbody2D>();
 
-		// Check the other colliding object's tag to know if it's
-		// indeed a player projectile
+        // Check the other colliding object's tag to know if it's
+        // indeed a player projectile
         if (collision.tag == "Laser")
         {
+            // Make gift fall down after collision
+            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
             // Destroy the alien game object
-            Destroy(gameObject);
-			
+            //Destroy(gameObject);
+
             // Destroy the projectile game object
             Destroy(collision.gameObject);
 			
 			// Play an audio clip in the scene and not attached to the alien
 			// so the sound keeps playing even after it's destroyed
             AudioSource.PlayClipAtPoint(destructionSFX, transform.position);
+        }
+
+        if(gift.transform.position.y >= floor)
+        {
+            // let fallen gifts stop on the floor once they hit it
+            gift.velocity = Vector2.zero;
+            gift.isKinematic = true;
+            transform.position = new Vector3(transform.position.x, floor, transform.position.z);
+            transform.SetParent(null); //disconnect from parent to stop movement in x-direction
+        }
+        
+        if (collision.tag == "Santa")
+        {
+            Debug.Log("santa collision");
         }
     }
 }
--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -77,4 +77,6 @@ public class EnemyController : MonoBehaviour
             }
         }
     }
+
+    
 }
